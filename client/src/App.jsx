ad5d4352e8fdb1be6ce8aef5addb7eb7f9af75c9
mohamed-yassine2904 <yassine2904@gmail.com>
2024-11-14 08:39:40 +0100

    commit b4 pull

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
 import Home from './components/Home/Home'; 
 import Navbar from './components/Home/Navbar'; 
+import Profile from './components/Home/Profile';
 
 function App() {
   return (
@@ -10,6 +11,12 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} /> */}
+
+        <Route path="/Profile" element={<Profile />} />
+    
       </Routes>
       
     </Router>
--- client/src/components/Home/Navbar.jsx
+++ client/src/components/Home/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import '../Home/Navbar.css'; 
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+    const navigate=useNavigate()
   return (
     <div className="navbar">
       <div className="nav-links">
-        <a href="#home">Home</a>
-        <a href="#lessons">Lessons</a>
+        <a onClick={()=>navigate("/")}>Home</a>
+        <a >Lessons</a>
       </div>
-      <div className="profile-icon">
+      <div className="profile-icon" onClick={()=>navigate("/Profile")} >
         <FontAwesomeIcon icon={faUserCircle} />
       </div>
     </div>
