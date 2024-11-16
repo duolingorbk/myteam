95e7b8b9046b3796d13ea103777f271089423cf7
MelekElmayy <melek.elmay123@gmail.com>
2024-11-16 15:57:48 +0100

    81

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';  // Import BrowserRouter
-// import App from './App.jsx'
-// import Lessons from './components/lessons pages/Lessons.jsx'
+import App from './App.jsx'
+import Lessons from './components/lessons pages/Lessons.jsx'
 import Questions from './components/Questions.jsx'
 // import Questions from './components/Questions.jsx'
 
@@ -11,9 +11,7 @@ import Questions from './components/Questions.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    {/* <App /> */}
-    {/* <Lessons /> */}
-    <Questions />
+    <App />
     </BrowserRouter>
   </StrictMode>,
 )
