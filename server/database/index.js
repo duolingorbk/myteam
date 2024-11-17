b2edc578a80ed805f1d55bb0de0cb67ae687400a
FatmaChirchi <chirchifatma@gmail.com>
2024-11-17 14:52:18 +0100

    to pull

--- server/database/index.js
+++ server/database/index.js
@@ -1,7 +1,7 @@
 const { Sequelize  } = require('sequelize');
 
 
-const sequelize = new Sequelize('speakeasy', 'root', 'sazitoun', {
+const sequelize = new Sequelize('speakeasy', 'root', 'aqw', {
   host: 'localhost',
   dialect: 'mysql' 
 });
